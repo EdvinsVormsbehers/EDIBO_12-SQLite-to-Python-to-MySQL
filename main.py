4bc325f3c3febb81a811f1b3b968458635a80f57
WhiTe <whiteedd@inbox.lv>
2021-07-06 13:21:09 +0300

    Made a function of making script for insert data in tables

--- main.py
+++ main.py
@@ -12,34 +12,37 @@ file_to_read_tracks = open('data_in_tracks.txt', 'r')
 
 file_to_write = open('data_out.sql', 'w')
 
-data = file_to_read_albums.read()
-print("Result after - file_to_read_albums.read():")
-print(type(data))
-print(data)
 
-data_lines = data.split('\n')[:-1]
-print("Result after - data.split(\'\n\')[:-1]:")
-print(type(data_lines))
-print(data_lines)
+def insert_table_data(file_to_read):
+    data = file_to_read.read()
+    # print("Result after - file_to_read_albums.read():")
+    # print(type(data))
+    # print(data)
 
-for line in data_lines[:-1]:
-    #print(line.split('|'))
-    fields = line.split('|')
+    data_lines = data.split('\n')[:-1]
+    # print("Result after - data.split(\'\n\')[:-1]:")
+    # print(type(data_lines))
+    # print(data_lines)
+
+    for line in data_lines[:-1]:
+        # print(line.split('|'))
+        fields = line.split('|')
+        file_to_write.write("(\'")
+        for field in fields[:-1]:
+            file_to_write.write(field)
+            file_to_write.write("\',\'")
+        file_to_write.write(fields[-1])
+        file_to_write.write("\'),")
+        file_to_write.write('\n')
+
+    fields = data_lines[-1].split('|')
     file_to_write.write("(\'")
     for field in fields[:-1]:
         file_to_write.write(field)
         file_to_write.write("\',\'")
     file_to_write.write(fields[-1])
-    file_to_write.write("\'),")
-    file_to_write.write('\n')
+    file_to_write.write("\');\n")
 
-fields = data_lines[-1].split('|')
-file_to_write.write("(\'")
-for field in fields[:-1]:
-    file_to_write.write(field)
-    file_to_write.write("\',\'")
-file_to_write.write(fields[-1])
-file_to_write.write("\');\n")
 
 file_to_read_albums.close()
 file_to_read_artists.close()
