db64097b8dca94614697f3ac563c60a2d6b7600d
WhiTe <whiteedd@inbox.lv>
2021-07-06 17:55:08 +0300

    Bug fix

--- main.py
+++ main.py
@@ -34,8 +34,10 @@ def insert_table_data(file_to_read):
         fields = line.split('|')
         file_to_write.write("(\'")
         for field in fields[:-1]:
+            field = field.replace("'", "")
             file_to_write.write(field)
             file_to_write.write("\',\'")
+        fields[-1] = fields[-1].replace("'", "")
         file_to_write.write(fields[-1])
         file_to_write.write("\'),")
         file_to_write.write('\n')
@@ -45,6 +47,7 @@ def insert_table_data(file_to_read):
     for field in fields[:-1]:
         file_to_write.write(field)
         file_to_write.write("\',\'")
+    fields[-1] = fields[-1].replace("'", "")
     file_to_write.write(fields[-1])
     file_to_write.write("\');\n")
     file_to_write.write('\n')
