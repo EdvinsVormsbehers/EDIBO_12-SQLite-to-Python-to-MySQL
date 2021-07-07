3c2224aeb39bf9f53dec7433c4947dfb41fe03e2
WhiTe <whiteedd@inbox.lv>
2021-07-07 16:41:43 +0300

    Fix a replace method for replacing ' to \\'

--- main.py
+++ main.py
@@ -22,22 +22,26 @@ def insert_table_data(file_to_read):
         # print(line.split('|'))
         fields = line.split('|')
         file_to_write.write("(\'")
+        i = 0
+        for field in fields:
+            fields[i] = field.replace("'", "\\'")
+            i += 1
         for field in fields[:-1]:
-            field = field.replace("'", "")
             file_to_write.write(field)
             file_to_write.write("\',\'")
-        fields[-1] = fields[-1].replace("'", "")
         file_to_write.write(fields[-1])
         file_to_write.write("\'),")
         file_to_write.write('\n')
 
     fields = data_lines[-1].split('|')
     file_to_write.write("(\'")
+    i = 0
+    for field in fields:
+        fields[i] = field.replace("'", "\\'")
+        i += 1
     for field in fields[:-1]:
-        field = field.replace("'", "")
         file_to_write.write(field)
         file_to_write.write("\',\'")
-    fields[-1] = fields[-1].replace("'", "")
     file_to_write.write(fields[-1])
     file_to_write.write("\');\n")
     file_to_write.write('\n')
